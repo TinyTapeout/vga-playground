fffdd951679669343d90f7c6aaa0f9e4a357845b
Uri Shaked <uri@urishaked.com>
2024-05-21 13:38:27 +0300

    feat(examples): add logo

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -1,5 +1,6 @@
 import { Project } from './Project';
 import { balls } from './balls';
 import { stripes } from './stripes';
+import { logo } from './logo';
 
-export const examples: Project[] = [stripes, balls];
+export const examples: Project[] = [stripes, balls, logo];
