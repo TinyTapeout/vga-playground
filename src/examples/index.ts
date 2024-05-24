908c745eb8455f2fb4aa0685435be5639fee8810
Ciro Cattuto <ciro.cattuto@gmail.com>
2024-05-24 17:54:40 +0200

    Conway's Game of Life example

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -2,5 +2,6 @@ import { Project } from './Project';
 import { balls } from './balls';
 import { stripes } from './stripes';
 import { logo } from './logo';
+import { conway } from './conway';
 
-export const examples: Project[] = [stripes, balls, logo];
+export const examples: Project[] = [stripes, balls, logo, conway];
