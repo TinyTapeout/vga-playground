ff99ea36362bbfd40e92d936d6b0116d5231fb90
Uri Shaked <uri@urishaked.com>
2025-02-05 19:20:05 +0200

    refactor(examples/gamepad): extra gamepad_pmod driver code to a separate file

--- src/examples/gamepad/index.ts
+++ src/examples/gamepad/index.ts
@@ -1,12 +1,13 @@
 import hvsync_generator_v from '../common/hvsync_generator.v?raw';
 import project_v from './project.v?raw';
+import gamepad_pmod_v from '../common/gamepad_pmod.v?raw';
 
 export const gamepad = {
   name: 'Gamepad',
   author: 'Uri Shaked',
   topModule: 'tt_um_vga_example',
   sources: {
-    'project.v': project_v,
+    'project.v': project_v + '\n\n' + gamepad_pmod_v,
     'hvsync_generator.v': hvsync_generator_v,
   },
 };
